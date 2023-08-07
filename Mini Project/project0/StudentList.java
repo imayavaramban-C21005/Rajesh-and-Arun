0a1b329903a0009575c40a18da5cc9ba3b4fb85f
CCUBIC007 <arun.subramanian@ccubic.in>
2023-08-07 14:45:30 +0530

    field name modified

--- Mini Project/project0/StudentList.java
+++ Mini Project/project0/StudentList.java
@@ -1,12 +1,11 @@
 package project0;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class StudentList {
 
     Scanner scan = new Scanner(System.in);
-    Student [] list = new Student[10];
+    Student [] list = new Student[3];
     Student student = new Student();
     int count = 0;
     public void regiteringStudent(){
@@ -18,18 +17,20 @@ public class StudentList {
 
             }
             else {
-                System.out.println("student "+i);
+                System.out.println("student "+ (i+1));
                 System.out.println("Enter Your Name : ");
                 student.setName(scan.next());
-                System.out.println("Enter Your Age : ");
-                student.setAge(Integer.parseInt(scan.next()));
+                System.out.println("Enter Your cutoff : ");
+                student.setCutoff(Integer.parseInt(scan.next()));
                 System.out.println("Enter Your RegisterNumber : ");
                 student.setRegisterNumber(Integer.parseInt(scan.next()));
-                list[i] = new Student(student.getName(), student.getAge(), student.getRegisterNumber());
+                list[i] = new Student(student.getName(), student.getCutoff(), student.getRegisterNumber());
             }
         }
         for (Student item : list ){
-            System.out.println(item.getName());
+            System.out.println(item.getName() + " - ");
+            System.out.println(item.getCutoff() + " - ");
+            System.out.println(item.getRegisterNumber());
         }
 //        Arrays.stream(list)
 //                .map(Student::getName)
